a8ff21325daf30a96f6a2d2d06b429d1b1a8a625
Michal R <vad.sol@proton.me>
2026-02-28 19:14:30 +0100

    aya-ebpf: Add methods returning slices to `DirectPacketAccess`
    We know the exact address and length of packet data, yet our API that
    returns only pointers. The way of using that API that we officially
    suggest is pointer arithmetics and pointer casting, which involves
    unsafe operations.
    
    Given that `network-types` supports zero-copy deserialization from
    bytes using `wincode`, the new API allows to parse packets with no
    unsafe code.

--- ebpf/aya-ebpf/src/programs/packet.rs
+++ ebpf/aya-ebpf/src/programs/packet.rs
@@ -1,3 +1,5 @@
+use core::slice;
+
 /// Trait implemented by program context types which support the direct packet
 /// access.
 pub trait DirectPacketAccess {
@@ -6,4 +8,28 @@ pub trait DirectPacketAccess {
 
     /// Returns the raw address immediately after the metadata.
     fn data_end(&self) -> usize;
+
+    /// Returns the raw address of data and its length.
+    #[inline]
+    fn data_with_len(&self) -> (usize, usize) {
+        let data = self.data();
+        let len = self.data_end() - data;
+        (data, len)
+    }
+
+    /// Returns the data as a slice of bytes.
+    #[inline]
+    fn data_bytes(&self) -> &[u8] {
+        let (data, len) = self.data_with_len();
+        // SAFETY: We know the exact address and length of the data.
+        unsafe { slice::from_raw_parts(data, len) }
+    }
+
+    /// Returns the data as a mutable slice of bytes.
+    #[inline]
+    fn data_bytes_mut(&mut self) -> &mut [u8] {
+        let (data, len) = self.data_with_len();
+        // SAFETY: We know the exact address and length of the data.
+        unsafe { slice::from_raw_parts_mut(data, len) }
+    }
 }
